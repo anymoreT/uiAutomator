29818af1f5a099ac2a02a794c121a5687d950403
huangyong <huangyong@pxsj.com>
2017-02-21 15:50:31 +0800

    first commit

---
+++ app/src/androidTest/java/qa/com/uiaumationdemo/UiTest.java
@@ -0,0 +1,18 @@
+package qa.com.uiaumationdemo;
+
+import android.support.test.uiautomator.UiAutomatorTestCase;
+import android.support.test.uiautomator.UiDevice;
+import android.support.test.uiautomator.UiObject;
+import android.support.test.uiautomator.UiObjectNotFoundException;
+import android.support.test.uiautomator.UiSelector;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+ public class UiTest  {
+         public void testDemo() throws UiObjectNotFoundException {
+             UiDevice ins = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
+
+          }
+    }
