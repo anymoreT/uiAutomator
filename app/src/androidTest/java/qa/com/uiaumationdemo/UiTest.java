0dcfe1df940fdc571b9e3aa054caed8be23838e2
benhuang <249773304@qq.com>
2017-02-27 15:55:31 +0800

    open app

--- app/src/androidTest/java/qa/com/uiaumationdemo/UiTest.java
+++ app/src/androidTest/java/qa/com/uiaumationdemo/UiTest.java
@@ -7,12 +7,37 @@ import android.support.test.uiautomator.UiObjectNotFoundException;
 import android.support.test.uiautomator.UiSelector;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
+import android.content.Context;
+import android.content.Intent;
+import org.junit.Test;
+import org.junit.Before;
 import org.junit.runner.RunWith;
 
 @RunWith(AndroidJUnit4.class)
  public class UiTest  {
+    private UiDevice mDevice;
+
+    @Before
+    public void before() {
+        // Initialize UiDevice instance
+        mDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
+
+        // Start from the home screen
+        mDevice.pressHome();
+        // open app
+        openApp("com.qunar.im.qchat");
+    }
+
+    public void openApp(String packageName) {
+        Context context = InstrumentationRegistry.getInstrumentation().getContext();
+        Intent intent = context.getPackageManager().getLaunchIntentForPackage(packageName);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        context.startActivity(intent);
+    }
+
+         @Test
          public void testDemo() throws UiObjectNotFoundException {
-             UiDevice ins = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
+
 
           }
     }
